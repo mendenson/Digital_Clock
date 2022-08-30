5818e5506ec75573957c0eaa00b284d0fb7fb3d9
Mendenson Mota <mendenson@gmail.com>
2022-08-29 18:07:24 -0600

    update folders

--- Visual_Basic/Visual_Basic/Visual_Basic/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class frmClocks
-    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrTimer.Tick
-        Clock1.Text = TimeString ' 24 hour time '
-        Clock2.Text = DateTime.Now.ToString("hh:mm:ss tt") ' Time with AM/PM '
-    End Sub
-End Class
